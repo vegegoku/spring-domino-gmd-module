fa76e5bc917cec595563196239e93f4c48661cd2
LeshaRB <lesharb@gmail.com>
2018-02-24 20:11:51 +0300

    First step maven2gradle

--- portal-items/todolist-items-backend/src/main/java/com/progressoft/brix/domino/sample/items/server/handlers/ClearAllHandler.java
+++ portal-items/todolist-items-backend/src/main/java/com/progressoft/brix/domino/sample/items/server/handlers/ClearAllHandler.java
@@ -6,20 +6,20 @@ import com.progressoft.brix.domino.api.server.handler.RequestHandler;
 import com.progressoft.brix.domino.api.shared.request.VoidRequest;
 import com.progressoft.brix.domino.sample.items.server.TodoItemsStore;
 import com.progressoft.brix.domino.sample.items.shared.response.RemoveResponse;
-
 import java.util.logging.Logger;
 
 @Handler(value = "todo/clear")
 public class ClearAllHandler implements RequestHandler<VoidRequest, RemoveResponse> {
-    private static final Logger LOGGER = Logger.getLogger(ClearAllHandler.class.getName());
 
-    @Override
-    public void handleRequest(ExecutionContext<VoidRequest, RemoveResponse> executionContext) {
-        try {
-            TodoItemsStore.clear();
-            executionContext.end(new RemoveResponse(true));
-        } catch (Exception e) {
-            executionContext.end(new RemoveResponse(false));
-        }
+  private static final Logger LOGGER = Logger.getLogger(ClearAllHandler.class.getName());
+
+  @Override
+  public void handleRequest(ExecutionContext<VoidRequest, RemoveResponse> executionContext) {
+    try {
+      TodoItemsStore.clear();
+      executionContext.end(new RemoveResponse(true));
+    } catch (Exception e) {
+      executionContext.end(new RemoveResponse(false));
     }
+  }
 }
